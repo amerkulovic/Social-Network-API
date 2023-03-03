b1bf1626c43f016419959d7f1625306e5cd74eee
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-03 13:15:35 -0600

    created get and post route for Users

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const db = require("./config/connection");
 // Require model
-// const {  } = require("./models");
+const { User } = require("./models");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.post('/new-user', (req, res) => {
+  const newUser = new User({ username: req.body.username, email: req.body.email });
+  newUser.save();
+  if (newUser) {
+    res.status(201).json(newUser);
+  } else {
+    console.log('Uh Oh, something went wrong');
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+app.get('/all-users', (req, res) => {
+  User.find({}, (err, result) => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      console.log('Uh Oh, something went wrong');
+      res.status(500).json({ error: 'Something went wrong' });
+    }
+  });
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
