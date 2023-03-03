33e4cefea1711aea91d204a26b23480ad1cdde41
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-03 11:56:01 -0600

    created the User model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+let validateEmail = function (email) {
+  let response = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return response.test(email);
+};
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true, trim: true, required: true },
+    email: { type: String, unique: true, required: true, validate: [validateEmail, "Please fill a valid email address"], match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email address"] },
+    thought: [],
+    friends: [],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema
+  .virtual("friendCount")
+  .get(function () {
+    return this.friends.length;
+  })
+  .set(function () {
+    let count = this.friends.length;
+    this.set({ count });
+  });
+
+const User = mongoose.model("User", userSchema);
+
+const handleError = (err) => console.error(err);
+
+module.exports = User;
